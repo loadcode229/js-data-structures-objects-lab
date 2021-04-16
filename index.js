7850042a6391fd96e1ffdcec9b97d794040d6e31
loadcode229 <sepone229@gmail.com>
2021-04-15 21:13:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const driver = {};
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, { [key]: value });
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+};
+
+function deleteFromDriverByKey(driver, key) {
+    return Object.assign({});
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    Object.assign({}, driver);
+    delete driver[key];
+    return driver;
+};
